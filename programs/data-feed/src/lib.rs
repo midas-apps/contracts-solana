031d2a452986bb559af7cb23c86a59d24d464091
kostyamospan <kostya04042001@gmail.com>
2024-11-19 16:16:55 +0200

    fix: lib rs data feed calls

--- programs/data-feed/src/lib.rs
+++ programs/data-feed/src/lib.rs
@@ -15,4 +15,16 @@ pub mod data_feed {
     pub fn new_feed(ctx: Context<NewFeed>, authority: Pubkey, target_decimals: u8) -> Result<()> {
         new_feed::handle(ctx, authority, target_decimals)
     }
+
+    pub fn new_manual_feed(ctx: Context<NewManualFeed>, decimals: u8) -> Result<()> {
+        new_manual_feed::handle(ctx, decimals)
+    }
+
+    pub fn set_manual_mode(ctx: Context<SetManualMode>, enabled: bool) -> Result<()> {
+        set_manual_mode::handle(ctx, enabled)
+    }
+
+    pub fn set_manual_price(ctx: Context<SetManualPrice>, new_price: u64) -> Result<()> {
+        set_manual_price::handle(ctx, new_price)
+    }
 }
